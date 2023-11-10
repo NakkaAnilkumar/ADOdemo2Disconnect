b5a51ec1dbfcfe10c48a48200eec50e2ddb4581b
NakkaAnilkumar <anilsunny1999@gmail.com>
2023-11-10 16:15:36 +0530

    Add project files.

---
+++ ADOdemo2/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace DisconnectedDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnfind = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtskillid = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txttype = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(452, 72);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(267, 182);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(88, 334);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 1;
+            this.btnInsert.Text = "Insert Record";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter SkillID";
+            // 
+            // btnfind
+            // 
+            this.btnfind.AutoSize = true;
+            this.btnfind.Location = new System.Drawing.Point(50, 135);
+            this.btnfind.Name = "btnfind";
+            this.btnfind.Size = new System.Drawing.Size(103, 16);
+            this.btnfind.TabIndex = 3;
+            this.btnfind.Text = "Enter SkillName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Enter SkillType";
+            // 
+            // txtskillid
+            // 
+            this.txtskillid.Location = new System.Drawing.Point(169, 82);
+            this.txtskillid.Name = "txtskillid";
+            this.txtskillid.Size = new System.Drawing.Size(100, 22);
+            this.txtskillid.TabIndex = 5;
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(169, 129);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(100, 22);
+            this.txtname.TabIndex = 6;
+            // 
+            // txttype
+            // 
+            this.txttype.Location = new System.Drawing.Point(169, 181);
+            this.txttype.Name = "txttype";
+            this.txttype.Size = new System.Drawing.Size(100, 22);
+            this.txttype.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(216, 334);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Find";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(345, 334);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(473, 334);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 10;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txttype);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.txtskillid);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnfind);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label btnfind;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtskillid;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txttype;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ ADOdemo2/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Xml.Linq;
+
+
+namespace DisconnectedDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            SqlConnection cn = new SqlConnection("Data Source=SUNNYLAPPY\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+            SqlDataAdapter da = new SqlDataAdapter("select * from TechSkills", cn);
+            DataSet ds = new DataSet();
+            da.MissingSchemaAction = MissingSchemaAction.AddWithKey;//PrimaryKey/Foreign key
+            da.Fill(ds, "TechSkills");//After execution of this line, database is disconnected
+
+            DataTable dt = new DataTable();
+            dt = ds.Tables["TechSkills"];
+            dataGridView1.DataSource = dt;
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            SqlConnection cn = new SqlConnection("Data Source=SUNNYLAPPY\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+            SqlDataAdapter da = new SqlDataAdapter("select * from TechSkills", cn);
+            DataSet ds = new DataSet();
+            da.MissingSchemaAction = MissingSchemaAction.AddWithKey;//PrimaryKey/Foreign key
+            da.Fill(ds, "TechSkills");//After execution of this line, database is disconnected
+
+            DataRow myrow = ds.Tables["TechSkills"].NewRow();
+
+            myrow["SkillName"] = txtname.Text.Trim();
+            myrow["SkillType"] = txttype.Text.Trim();
+
+            ds.Tables["TechSkills"].Rows.Add(myrow);
+
+            //Reconnect to db server--submitting the changes
+            SqlCommandBuilder bldr = new SqlCommandBuilder(da);//insert
+            da.Update(ds.Tables["TechSkills"]);
+            MessageBox.Show("Added Successfully...");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private void txtskillid_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void txtskillid_MouseHover(object sender, EventArgs e)
+        {
+            txtskillid.ReadOnly = false;
+        }
+
+        private void txtname_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection cn = new SqlConnection("Data Source=SUNNYLAPPY\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+                SqlDataAdapter da = new SqlDataAdapter("select * from TechSkills", cn);
+                DataSet ds = new DataSet();
+                da.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+
+                // Open the connection explicitly
+                cn.Open();
+
+                da.Fill(ds, "TechSkills");
+
+                DataRow found = ds.Tables["TechSkills"].Rows.Find(Convert.ToInt32(txtskillid.Text));
+                if (found != null)
+                {
+                    found["SkillName"] = txtname.Text.Trim();
+                    found["SkillType"] = txttype.Text.Trim();
+
+                    // Update the changes back to the database
+                    SqlCommandBuilder bldr = new SqlCommandBuilder(da);
+                    da.Update(ds, "TechSkills");
+
+                    // Close the connection
+                    cn.Close();
+
+                    // Reload data
+                    Form1_Load(sender, e);
+                    MessageBox.Show("Updated Successfully...");
+                }
+                else
+                {
+                    throw new SkillNotFoundException("Please check Skill ID. Enter a valid Skill ID.");
+                }
+            }
+            catch (SkillNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+
+        }
+
+        private void btnfind_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                SqlConnection cn = new SqlConnection("Data Source=SUNNYLAPPY\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+                SqlDataAdapter da = new SqlDataAdapter("select * from TechSkills", cn);
+                DataSet ds = new DataSet();
+                da.MissingSchemaAction = MissingSchemaAction.AddWithKey;//PrimaryKey/Foreign key
+
+                da.Fill(ds, "TechSkills");//After execution of this line, database is disconnected
+
+                DataRow found = ds.Tables["TechSkills"].Rows.Find(Convert.ToInt32(txtskillid.Text));
+
+                if (found != null)
+                {
+                    txtname.Text = found["SkillName"].ToString();
+                    txttype.Text = found["SkillType"].ToString();
+
+                }
+                else
+                {
+                    txtname.Text = "";
+                    txttype.Text = "";
+                    throw new SkillNotFoundException("no record found with this id");
+
+
+                }
+            }
+            catch (SkillNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                SqlConnection cn = new SqlConnection("Data Source=SUNNYLAPPY\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+                SqlDataAdapter da = new SqlDataAdapter("select * from TechSkills", cn);
+                DataSet ds = new DataSet();
+                da.MissingSchemaAction = MissingSchemaAction.AddWithKey;//PrimaryKey/Foreign key
+
+                da.Fill(ds, "TechSkills");//After execution of this line, database is disconnected
+
+                DataRow found = ds.Tables["TechSkills"].Rows.Find(Convert.ToInt32(txtskillid.Text));
+
+                if (found != null)
+                {
+                    txtname.Text = found["SkillName"].ToString();
+                    txttype.Text = found["SkillType"].ToString();
+                    DialogResult dialogresult = MessageBox.Show("Found R u sure to delete?", "Confirmation from User", MessageBoxButtons.OKCancel);
+                    if (DialogResult.OK == dialogresult)
+                    {
+                        found.Delete();
+                        SqlCommandBuilder bldr = new SqlCommandBuilder(da);
+                        da.Update(ds.Tables["TechSkills"]);
+                        MessageBox.Show("Deleted Successfully....");
+                        txtskillid.Text = "";
+                        txtname.Text = "";
+                        txttype.Text = "";
+
+                        Form1_Load(sender, e);
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ok... I am not deleting");
+                    }
+
+
+                }
+                else
+                {
+                    txtname.Text = "";
+                    txttype.Text = "";
+                    throw new SkillNotFoundException("no record found with this id");
+
+
+                }
+            }
+            catch (SkillNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
